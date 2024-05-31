22864a3281b1ab99489caf234bbf1d678cffcc61
Robert Zhang <louirobert@gmail.com>
2024-05-31 10:00:22 +0800

    Update PowerShell_profile.ps1

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,25 +1,52 @@
+# Aliases
+
+Set-Alias -Name l -Value Get-ChildItem
+Set-Alias -Name vi -Value vim
+
 # Autocomplete git command
 # See https://github.com/dahlbyk/posh-git
 # Install the module by
 # PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
-
 Import-Module posh-git
 
 function gs {
     git status $($args -join ' ')
 }
 
-function gc {
+function gc_ {
     git commit $($args -join ' ')
 }
+
+# Override predefined alias gc
+# Note: The AllScope option cannot be removed from the original definition
+Set-Alias -Name gc -Value gc_ -Option AllScope -Force
 
 function gb {
     git branch $($args -join ' ')
 }
 
+function gd {
+    git diff -U10 $($args -join ' ')
+}
+
+function gl_ {
+    git log $($args -join ' ')
+}
+
+# Override predefined alias gl
+# Note: The AllScope option cannot be removed from the original definition
+Set-Alias -Name gl -Value gl_ -Option AllScope -Force
+
+function gss {
+    git shortlog -s --no-merges $($args -join ' ')
+}
+
+function gls {
+    git ls-files $($args -join ' ')
+}
+
 # Autocomplete dotnet command
 # See https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete#powershell
-
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
         dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
@@ -32,7 +59,6 @@ Set-Alias -Name dotnet -Value dotnet.exe
 # Autocomplete docker command
 # See https://github.com/matt9ucci/DockerCompletion
 # Install-Module DockerCompletion -Scope CurrentUser
-
 Import-Module DockerCompletion
 
 Set-Alias -Name docker -Value docker.exe
